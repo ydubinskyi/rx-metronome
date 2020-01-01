02da10002a4c14d8a5f7fcaabb4799b00963bd34
ydubinskyi <slavabilaz@gmail.com>
2020-01-01 17:14:59 +0100

    add base service-worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,8 +2,9 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
-  plugins: [new CleanWebpackPlugin(), new CompressionPlugin()],
+  plugins: [new CleanWebpackPlugin(), new CompressionPlugin(), new WorkboxPlugin.GenerateSW()],
 });
