11f0eb48e4adec7a2f88633f32bc6b0c55f92d17
ydubinskyi <slavabilaz@gmail.com>
2020-01-12 21:01:35 +0100

    used static manifest and icons

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,7 @@
-const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const CompressionPlugin = require('compression-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -14,21 +12,6 @@ module.exports = merge(common, {
       skipWaiting: true,
       navigateFallback: '/index.html',
       navigateFallbackWhitelist: [/^(?!\/__)/],
-    }),
-    new WebpackPwaManifest({
-      name: 'rx-metronome',
-      short_name: 'Metronome',
-      theme_color: '#302ae6',
-      background_color: '#302ae6',
-      display: 'standalone',
-      Scope: '/',
-      start_url: '/',
-      icons: [
-        {
-          src: path.resolve('src/assets/images/icon.png'),
-          sizes: [96, 128, 192, 256, 384, 512],
-        },
-      ],
     }),
   ],
 });
