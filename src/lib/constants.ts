ed925be7ca19583d2ddd9b7f35daa383af86fbba
ydubinskyi <slavabilaz@gmail.com>
2020-01-14 19:52:15 +0100

    moved tempo terms to constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,3 +4,96 @@ export const initState = {
   counter: 0,
   isTicking: false,
 };
+
+export const tempoTerms = [
+  {
+    from: 0,
+    to: 20,
+    name: 'Larghissimo',
+  },
+  {
+    from: 20,
+    to: 45,
+    name: 'Grave',
+  },
+  {
+    from: 40,
+    to: 60,
+    name: 'Largo',
+  },
+  {
+    from: 45,
+    to: 60,
+    name: 'Lento',
+  },
+  {
+    from: 60,
+    to: 66,
+    name: 'Larghetto',
+  },
+  {
+    from: 66,
+    to: 76,
+    name: 'Adagio',
+  },
+  {
+    from: 70,
+    to: 80,
+    name: 'Adagietto',
+  },
+  {
+    from: 76,
+    to: 108,
+    name: 'Andante',
+  },
+  {
+    from: 80,
+    to: 108,
+    name: 'Andantino',
+  },
+  {
+    from: 83,
+    to: 88,
+    name: 'Marcia moderato',
+  },
+  {
+    from: 92,
+    to: 112,
+    name: 'Andante moderato',
+  },
+  {
+    from: 108,
+    to: 120,
+    name: 'Moderato',
+  },
+  {
+    from: 116,
+    to: 120,
+    name: 'Allegro moderato',
+  },
+  {
+    from: 120,
+    to: 156,
+    name: 'Allegro',
+  },
+  {
+    from: 156,
+    to: 176,
+    name: 'Vivace',
+  },
+  {
+    from: 172,
+    to: 176,
+    name: 'Vivacissimo',
+  },
+  {
+    from: 168,
+    to: 200,
+    name: 'Presto',
+  },
+  {
+    from: 200,
+    to: 300,
+    name: 'Prestissimo',
+  },
+];
--- src/lib/rx-tempo-text.element.ts
+++ src/lib/rx-tempo-text.element.ts
@@ -1,97 +1,6 @@
 import {css, customElement, html, LitElement, property} from 'lit-element';
 
-const terms = [
-  {
-    from: 0,
-    to: 20,
-    name: 'Larghissimo',
-  },
-  {
-    from: 20,
-    to: 45,
-    name: 'Grave',
-  },
-  {
-    from: 40,
-    to: 60,
-    name: 'Largo',
-  },
-  {
-    from: 45,
-    to: 60,
-    name: 'Lento',
-  },
-  {
-    from: 60,
-    to: 66,
-    name: 'Larghetto',
-  },
-  {
-    from: 66,
-    to: 76,
-    name: 'Adagio',
-  },
-  {
-    from: 70,
-    to: 80,
-    name: 'Adagietto',
-  },
-  {
-    from: 76,
-    to: 108,
-    name: 'Andante',
-  },
-  {
-    from: 80,
-    to: 108,
-    name: 'Andantino',
-  },
-  {
-    from: 83,
-    to: 88,
-    name: 'Marcia moderato',
-  },
-  {
-    from: 92,
-    to: 112,
-    name: 'Andante moderato',
-  },
-  {
-    from: 108,
-    to: 120,
-    name: 'Moderato',
-  },
-  {
-    from: 116,
-    to: 120,
-    name: 'Allegro moderato',
-  },
-  {
-    from: 120,
-    to: 156,
-    name: 'Allegro',
-  },
-  {
-    from: 156,
-    to: 176,
-    name: 'Vivace',
-  },
-  {
-    from: 172,
-    to: 176,
-    name: 'Vivacissimo',
-  },
-  {
-    from: 168,
-    to: 200,
-    name: 'Presto',
-  },
-  {
-    from: 200,
-    to: 300,
-    name: 'Prestissimo',
-  },
-];
+import {tempoTerms} from './constants';
 
 @customElement('rx-tempo-text')
 class RxTempoTextElement extends LitElement {
@@ -101,7 +10,7 @@ class RxTempoTextElement extends LitElement {
   public get bmpText(): string {
     const bpm = this.beatsPerMinute;
 
-    return terms
+    return tempoTerms
       .filter((term) => bpm >= term.from && bpm <= term.to)
       .map((term) => term.name)
       .join(', ');
