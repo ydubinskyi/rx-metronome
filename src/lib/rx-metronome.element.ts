773848be7e2fae76b3e10745b2a0b3baf489b825
ydubinskyi <slavabilaz@gmail.com>
2020-01-16 21:46:19 +0100

    add tap tempo button

--- src/lib/rx-metronome.element.ts
+++ src/lib/rx-metronome.element.ts
@@ -1,18 +1,18 @@
 import {TextField} from '@material/mwc-textfield';
 import {css, customElement, eventOptions, html, LitElement, property} from 'lit-element';
 import {BehaviorSubject, Subject} from 'rxjs';
-import {distinctUntilChanged, pluck, takeUntil} from 'rxjs/operators';
+import {bufferCount, distinctUntilChanged, filter, map, pluck, takeUntil, timeInterval} from 'rxjs/operators';
 
-import {initState, MAX_TEMPO_VALUE, MIN_TEMPO_VALUE} from './constants';
+import {initState, MAX_TEMPO_VALUE, MIN_TEMPO_VALUE, TACK_FREQUENCY, TICK_FREQUENCY} from './constants';
 import {Command} from './types/command.type';
 import {IMetronomeState} from './types/metronome-state.interface';
 
 import '@material/mwc-button';
 import '@material/mwc-icon-button';
 import '@material/mwc-textfield';
+import './rx-tempo-pendulum.element';
 import './rx-tempo-text.element';
 import './rx-ticker.element';
-import './rx-tempo-pendulum.element';
 
 type HTMLElementEvent<T extends HTMLElement> = Event & {
   target: T;
@@ -33,6 +33,7 @@ class RxMetronomeElement extends LitElement {
   public counter: number;
 
   private metronomeState$: BehaviorSubject<IMetronomeState> = new BehaviorSubject(initState);
+  private tapTempoSubject$: Subject<void> = new Subject();
   private stateWorker: Worker;
 
   private isTicking$ = this.metronomeState$.pipe(pluck('isTicking'), distinctUntilChanged<boolean>());
@@ -55,6 +56,8 @@ class RxMetronomeElement extends LitElement {
     this.unsubscribe$.complete();
     this.audioContext.close();
     this.stateWorker.terminate();
+    this.metronomeState$.complete();
+    this.tapTempoSubject$.complete();
 
     super.disconnectedCallback();
   }
@@ -75,6 +78,11 @@ class RxMetronomeElement extends LitElement {
   }
 
   @eventOptions({passive: true})
+  public onTapTempoClick() {
+    this.tapTempoSubject$.next();
+  }
+
+  @eventOptions({passive: true})
   public onPlusOneClick() {
     if (this.beatsPerMinute < MAX_TEMPO_VALUE) {
       this.dispatchCommand({beatsPerMinute: this.beatsPerMinute + 1});
@@ -90,6 +98,15 @@ class RxMetronomeElement extends LitElement {
 
   @eventOptions({passive: true})
   public onBeatsPerMinuteChange({target: {value}}: HTMLElementEvent<TextField>) {
+    this.dispatchCommand({beatsPerMinute: this.validateBeatsPerMinute(value)});
+  }
+
+  @eventOptions({passive: true})
+  public onBeatsPerBarChange({target: {value}}: HTMLElementEvent<TextField>) {
+    this.dispatchCommand({beatsPerBar: Number(value)});
+  }
+
+  public validateBeatsPerMinute(value: string | number): number {
     let beatsPerMinute = Number(value);
 
     if (beatsPerMinute === undefined) {
@@ -100,12 +117,7 @@ class RxMetronomeElement extends LitElement {
       beatsPerMinute = MAX_TEMPO_VALUE;
     }
 
-    this.dispatchCommand({beatsPerMinute});
-  }
-
-  @eventOptions({passive: true})
-  public onBeatsPerBarChange({target: {value}}: HTMLElementEvent<TextField>) {
-    this.dispatchCommand({beatsPerBar: Number(value)});
+    return beatsPerMinute;
   }
 
   static get styles() {
@@ -143,9 +155,22 @@ class RxMetronomeElement extends LitElement {
         width: calc(100% - 16px);
       }
 
-      .buttons mwc-button,
+      .control-buttons {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        width: 100%;
+      }
+
+      .control-buttons mwc-button,
       .config-inputs mwc-textfield {
-        margin: 0 4px;
+        margin-left: 4px;
+        margin-right: 4px;
+      }
+
+      .tap-tempo-btn {
+        width: 100%;
+        margin-bottom: 16px;
       }
 
       mwc-button {
@@ -211,7 +236,14 @@ class RxMetronomeElement extends LitElement {
           @change="${this.onBeatsPerBarChange}"
         ></mwc-textfield>
       </div>
-      <div class="buttons">
+      <div class="control-buttons">
+        <mwc-button
+          outlined
+          class="tap-tempo-btn"
+          label="Tap the tempo"
+          icon="hourglass_empty"
+          @click="${this.onTapTempoClick}"
+        ></mwc-button>
         ${this.isTicking
           ? html`
               <mwc-button outlined label="stop" icon="stop" @click="${this.onStopClick}"></mwc-button>
@@ -243,9 +275,24 @@ class RxMetronomeElement extends LitElement {
       this.counter = value;
 
       if (value !== 0) {
-        this.playSound(value === 1 ? 880 : 440, 0.07);
+        this.playSound(value === 1 ? TICK_FREQUENCY : TACK_FREQUENCY, 0.07);
       }
     });
+
+    this.tapTempoSubject$
+      .pipe(
+        timeInterval(),
+        filter(({interval}) => interval < 6000),
+        bufferCount(3, 2),
+        map((values) => {
+          const intervals = values.map((value) => value.interval);
+          const avrInterval = intervals.reduce((a, b) => a + b) / intervals.length;
+          const beatsPerMinute = Math.round((60 * 1000) / avrInterval);
+
+          return this.validateBeatsPerMinute(beatsPerMinute);
+        }),
+      )
+      .subscribe((beatsPerMinute) => this.dispatchCommand({beatsPerMinute}));
   }
 
   private playSound(frequency: number, length: number) {
