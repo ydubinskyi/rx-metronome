00a7656dbb1e906e3bc1af450c653a05fe3018fe
ydubinskyi <slavabilaz@gmail.com>
2020-02-19 23:46:03 +0100

    add tests for ticker

--- src/lib/test/rx-ticker.test.ts
+++ src/lib/test/rx-ticker.test.ts
@@ -1,15 +1,21 @@
-import {expect, fixture, html} from '@open-wc/testing';
+import {expect} from '@open-wc/testing';
 
 import {RxTickerElement} from '../rx-ticker.element';
 
 describe('rx-ticker', () => {
   let element: RxTickerElement;
 
-  it('works', async () => {
-    element = await fixture(html`
-      <rx-ticker></rx-ticker>
-    `);
+  beforeEach(() => {
+    element = new RxTickerElement();
+  });
 
+  it('works', async () => {
     expect(element).to.be.ok;
+  });
+
+  it('properly generate items', async () => {
+    element.beatsPerBar = 4;
+
+    expect(element.items).to.be.deep.equal([0, 1, 2, 3]);
   });
 });
