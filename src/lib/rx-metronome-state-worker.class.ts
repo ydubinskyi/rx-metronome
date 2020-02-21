41d32992f1937b43aff3336e45ce200be9f03c6f
ydubinskyi <slavabilaz@gmail.com>
2020-02-21 16:34:56 +0100

    wrapped state worker logic in class

---
+++ src/lib/rx-metronome-state-worker.class.ts
@@ -0,0 +1,46 @@
+import {BehaviorSubject, combineLatest, fromEvent, merge, NEVER, Observable, timer} from 'rxjs';
+import {distinctUntilChanged, map, pluck, scan, shareReplay, switchMap, withLatestFrom} from 'rxjs/operators';
+
+import {initState} from './constants';
+import {Command} from './types/command.type';
+import {IMetronomeState} from './types/metronome-state.interface';
+
+export class RxMetronomeStateWorker {
+  public context: Worker;
+
+  public commands$: Observable<Command>;
+  public metronomeStateCommandBus$: BehaviorSubject<Command> = new BehaviorSubject(initState);
+  public metronomeState$: Observable<IMetronomeState>;
+  public isTicking$: Observable<boolean>;
+  public beatsPerMinute$: Observable<number>;
+
+  public counterUpdateTrigger$: Observable<number>;
+
+  constructor(context: Worker) {
+    this.context = context;
+
+    this.commands$ = fromEvent<{data: Command}>(this.context, 'message').pipe(map((event) => event.data));
+    this.metronomeState$ = merge(this.metronomeStateCommandBus$, this.commands$).pipe(
+      scan((metronomeState: IMetronomeState, command) => ({...metronomeState, ...command})),
+      shareReplay(1),
+    );
+    this.isTicking$ = this.metronomeState$.pipe(pluck('isTicking'), distinctUntilChanged<boolean>());
+    this.beatsPerMinute$ = this.metronomeState$.pipe(pluck('beatsPerMinute'), distinctUntilChanged<number>());
+
+    this.counterUpdateTrigger$ = combineLatest([this.isTicking$, this.beatsPerMinute$]).pipe(
+      switchMap(([isTicking, beatsPerMinute]) => (isTicking ? timer(0, 1000 * (60 / beatsPerMinute)) : NEVER)),
+    );
+  }
+
+  public subscribeOnState() {
+    this.counterUpdateTrigger$.pipe(withLatestFrom(this.metronomeState$)).subscribe(([_, {beatsPerBar, counter}]) => {
+      this.metronomeStateCommandBus$.next({
+        counter: counter < beatsPerBar ? counter + 1 : 1,
+      });
+    });
+
+    this.metronomeState$.subscribe((state) => {
+      this.context.postMessage(state);
+    });
+  }
+}
--- src/lib/rx-state.worker.ts
+++ src/lib/rx-state.worker.ts
@@ -1,32 +1,5 @@
-import {BehaviorSubject, combineLatest, fromEvent, merge, NEVER, Observable, timer} from 'rxjs';
-import {distinctUntilChanged, map, pluck, scan, shareReplay, switchMap, withLatestFrom} from 'rxjs/operators';
-
-import {initState} from './constants';
-import {Command} from './types/command.type';
-import {IMetronomeState} from './types/metronome-state.interface';
+import {RxMetronomeStateWorker} from './rx-metronome-state-worker.class';
 
 const ctx: Worker = self as any;
-
-const commands$ = fromEvent<{data: Command}>(ctx, 'message').pipe(map((event) => event.data));
-const metronomeStateCommandBus$: BehaviorSubject<Command> = new BehaviorSubject(initState);
-const metronomeState$: Observable<IMetronomeState> = merge(metronomeStateCommandBus$, commands$).pipe(
-  scan((metronomeState: IMetronomeState, command) => ({...metronomeState, ...command})),
-  shareReplay(1),
-);
-
-const isTicking$ = metronomeState$.pipe(pluck('isTicking'), distinctUntilChanged<boolean>());
-const beatsPerMinute$ = metronomeState$.pipe(pluck('beatsPerMinute'), distinctUntilChanged<number>());
-
-const counterUpdateTrigger$ = combineLatest([isTicking$, beatsPerMinute$]).pipe(
-  switchMap(([isTicking, beatsPerMinute]) => (isTicking ? timer(0, 1000 * (60 / beatsPerMinute)) : NEVER)),
-);
-
-counterUpdateTrigger$.pipe(withLatestFrom(metronomeState$)).subscribe(([_, {beatsPerBar, counter}]) => {
-  metronomeStateCommandBus$.next({
-    counter: counter < beatsPerBar ? counter + 1 : 1,
-  });
-});
-
-metronomeState$.subscribe((state) => {
-  ctx.postMessage(state);
-});
+const workerInstance = new RxMetronomeStateWorker(ctx);
+workerInstance.subscribeOnState();
