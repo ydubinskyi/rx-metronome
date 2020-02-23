76d4e1a883c3b9eeeea99f4ec45a53f4d87cf259
ydubinskyi <slavabilaz@gmail.com>
2020-02-23 23:15:56 +0100

    add basic test to root element

--- src/components/root/root.element.spec.ts
+++ src/components/root/root.element.spec.ts
@@ -1,4 +1,5 @@
 import {expect} from '@open-wc/testing';
+import {stub} from 'sinon';
 
 import {AppRootElement} from './root.element';
 
@@ -11,5 +12,20 @@ describe('my-app', () => {
 
   it('works', () => {
     expect(element).to.be.ok;
+  });
+
+  describe('toggleDarkMode', () => {
+    beforeEach(() => {
+      stub(element, 'changeThemeColor');
+      stub(localStorage, 'setItem');
+    });
+
+    it('should toggle darkTheme prop', () => {
+      element.darkTheme = false;
+
+      element.toggleDarkMode();
+
+      expect(element.darkTheme).to.equal(true);
+    });
   });
 });
