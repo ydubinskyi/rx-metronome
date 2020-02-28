27fff42040ab0c437b3ebad13bff3cfe98ddba90
ydubinskyi <slavabilaz@gmail.com>
2020-02-28 18:19:12 +0100

    add base test for play sound mixin

---
+++ src/mixins/rx-play-sound.mixin.spec.ts
@@ -0,0 +1,27 @@
+import {expect} from '@open-wc/testing';
+import {restore, stub} from 'sinon';
+
+import {BaseClass} from '../test-utils';
+import {RxPlaySoundMixin} from './rx-play-sound.mixin';
+
+class ClassWithMixin extends RxPlaySoundMixin(BaseClass as any) {
+  constructor() {
+    super();
+  }
+}
+
+describe('RxPlaySoundMixin', () => {
+  let instance: ClassWithMixin;
+
+  beforeEach(() => {
+    instance = new ClassWithMixin();
+  });
+
+  afterEach(() => {
+    restore();
+  });
+
+  it('works', () => {
+    expect(instance).to.be.ok;
+  });
+});
