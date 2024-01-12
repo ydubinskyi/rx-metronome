ee4771e5a9333e7c5d4e143df56e80a025e32c2a
Yaroslav Dubinskyi <slavabilaz@gmail.com>
2024-01-12 18:01:09 +0100

    add prettier job

--- src/components/root/root.styles.ts
+++ src/components/root/root.styles.ts
@@ -37,7 +37,9 @@ export const styles = css`
     display: flex;
     border-radius: 4px;
     background: var(--bg-color);
-    box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14),
+    box-shadow:
+      0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+      0px 1px 1px 0px rgba(0, 0, 0, 0.14),
       0px 1px 3px 0px rgba(0, 0, 0, 0.12);
     max-width: 480px;
     margin: 16px auto;
